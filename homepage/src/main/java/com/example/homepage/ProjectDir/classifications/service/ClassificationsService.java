d7539976184824deb044969318604490c02ad14f
최희선 <todaysunny612@gmail.com>
2025-07-02 00:50:17 +0900

    recruit n post update

--- homepage/src/main/java/com/example/homepage/ProjectDir/classifications/service/ClassificationsService.java
+++
@@ -1,4 +0,0 @@
-package com.example.homepage.ProjectDir.classifications.service;
-
-public interface ClassificationsService {
-}
---
+++ src/main/java/Project/LIKELION/Repository/PostRepository.java
@@ -0,0 +1,12 @@
+package Project.LIKELION.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import Project.LIKELION.Entity.POST.PostEntity;
+
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+    // 어차피 포스트 엔티티에 태그 엔티티를 일대다로 매핑 시켜뒀으니까
+    //여기서는 그냥 Jpa로 포스트 엔티티만 다루면 됨.
+}
